823cc58cb07992a1523605385ce56db9b91f9709
dueroxette92 <87718970+dueroxette92@users.noreply.github.com>
2021-09-29 03:32:26 +0300

    commit on 'main'

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const profileName = document.querySelector('.profile__name_type_name');
 const profileFood = document.querySelector('.profile__name_type_des');
 const openEditProfileButton = document.querySelector('.profile__edit-button');
+const closeBtn = document.querySelector('.popup__close')
 
 const profilePopup = document.querySelector('.popup_type_profile');
 const popupInputName = profilePopup.querySelector('.popup__input_type_name');
 const popupInputFood = profilePopup.querySelector('.popup__input_type_des');
 const popupSaveProfileButton = profilePopup.querySelector('.popup__button');
 
+
+const heart = document.querySelector('.photo__btnheart');
+const whiteHeart = '\u2661';
+const blackHeart = '\u2665';
+
+heart.addEventListener('click', toggle);
+
+function toggle() {
+    const like = heart.textContent;
+    if (like == whiteHeart) {
+        heart.textContent = blackHeart;
+    } else {
+        heart.textContent = whiteHeart;
+    }
+}
+
 function openPopup(popup) {
     popup.classList.add('popup__is-opened');
+
 }
 
 function closePopup(popup) {
@@ -25,6 +43,7 @@ openEditProfileButton.addEventListener('click', () => {
     popupInputName.value = profileName.textContent;
     popupInputFood.value = profileFood.textContent;
     openPopup(profilePopup);
+
 });
 
 popupSaveProfileButton.addEventListener('click', (event) => {
